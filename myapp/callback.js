3e9c8343678506e794467894b297cd1b7e932ab4
Pawan Saroj <pawansaroj6783@gmail.com>
2020-09-01 09:59:47 +0530

    first commit

---
+++ myapp/callback.js
@@ -0,0 +1,1 @@
+setTimeout(() => console.log("message"),3000);
---
+++ myapp/index.js
@@ -0,0 +1,4 @@
+const fun = require("testmoduletorevertstring");
+
+let revstring = fun.revert("hello world");
+console.log(revstring);
